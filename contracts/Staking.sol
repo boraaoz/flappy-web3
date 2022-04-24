e4f159e93539d3bcb16fa5eeb0b1e53d7178d378
Furkan Akal <30433306+furkanakal@users.noreply.github.com>
2022-04-24 04:20:48 +0300

    Update Staking.sol

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -2,8 +2,8 @@
 
 pragma solidity ^0.8.0;
 
-import "/node_modules/@openzeppelin/contracts/access/Ownable.sol";
-import "/node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Staking is Ownable {
 
